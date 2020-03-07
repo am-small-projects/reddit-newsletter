0add208220a663afeec3a3de00d80ada14893ed8
afshanaman <afshan.aman@gmail.com>
2020-03-07 15:14:18 -0700

    added husky and prettier config

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+
+const app = express();
 const PORT = 3000;
 
-app.use('/simple', require('./router.simple'))
+// app.use('/simple', require('./router.simple'))
 
 // final error handling
 app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-  app.listen(PORT, () => {
-    console.log(`
+app.listen(PORT, () => {
+  console.log(`
       App is Listening on port ${PORT}
       ENV: ${process.env.NODE_ENV}
       URL: http://localhost:${PORT}`);
-  });
+});
